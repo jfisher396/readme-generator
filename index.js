c6c0c2c69c47254c399680a2f37c4f0a0b48dd1a
James Fisher <jfisher396@hotmail.com>
2020-07-06 09:51:52 -0700

    added more questions and started functions

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
+// Inquirer variable to allow use of NPM Inquirer module
 const inquirer = require("inquirer");
+// File service variable that allows for reading/writing of files
 const fs = require('fs');
+// Variable to connect generateMarkdown function to application
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = inquirer.prompt([{
+const questions = [{
         type: "input",
         name: "title",
         message: "What is the title of your project?"
     },
     {
         type: "input",
-        message: "Please enter a description of your project:",
+        message: "Please enter a brief description of your project:",
         name: "description",
     },
     {
-        type: "list",
-        message: "What is your preferred method of communication?",
-        name: "contact",
-        choices: [
-            "email",
-            "phone",
-            "telekinesis"
-        ]
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Please describe how we can use this program?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "How can someone contribute to your project?",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "Please enter any testing protocols you used for your project?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "Questions?",
+        name: "questions",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your GitHub email?",
     }
+]
 
-])
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let readMeData = generateMarkdown(data);
+    fs.writeFile(fileName, readMeData, function (err) {
 
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+
+    });
+}
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions);
 }
 
 // function call to initialize program
