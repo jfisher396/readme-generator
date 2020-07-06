8f25dabbe5d59c3978de01a983a104a80b7fe343
James Fisher <jfisher396@hotmail.com>
2020-07-05 22:28:13 -0700

    started adding questions

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
-];
+// array of questions for user
+const questions = inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        message: "Please enter a description of your project:",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "What is your preferred method of communication?",
+        name: "contact",
+        choices: [
+            "email",
+            "phone",
+            "telekinesis"
+        ]
+    }
+
+])
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
