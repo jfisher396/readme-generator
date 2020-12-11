ec809c13611045475a402ee10d561c80975b28c6
James Fisher <jfisher396@hotmail.com>
2020-12-11 11:52:30 -0800

    moved questions to a new file

---
+++ utils/questions.js
@@ -0,0 +1,76 @@
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    message: "Please enter a one-sentence description of your project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please tell us about your project:",
+    name: "about",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions for your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the link to clone the repo?",
+    name: "clone",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select the license you used for this project.",
+    choices: [
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNU LGPLv3",
+      "Mozilla",
+      "MIT",
+      "Apache",
+      "Boost",
+    ],
+  },
+  {
+    type: "input",
+    message: "Please enter any testing protocols you used for your project?",
+    name: "test",
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "userEmail",
+    message: "What is your GitHub email?",
+  },
+  {
+    type: "input",
+    name: "URL",
+    message: "What is the URL of the live site?",
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "What is the URL of the github repo?",
+  },
+];
+
+module.exports = {
+  questions:questions,
+};
