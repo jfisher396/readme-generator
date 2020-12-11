fdec150c1be4386788458c59741db81e2e99c00e
James Fisher <jfisher396@hotmail.com>
2020-12-11 11:35:17 -0800

    moved the badge function to its own file

--- index.js
+++ index.js
@@ -4,26 +4,9 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 // Variable to connect generateMarkdown function to application
 const generateMarkdown = require("./utils/generateMarkdown");
+const licenseBadge = require("./utils/licenseBadge").licenseBadge;
 
 
-// This function will determine badges based on license selection.
-function licenseBadge(value) {
-    if (value === "GNU AGPLv3") {
-        return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-    } else if (value === "GNU GPLv3") {
-        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-    } else if (value === "GNU LGPLv3") {
-        return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-    } else if (value === "Mozilla") {
-        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-    } else if (value === "MIT") {
-        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    } else if (value === "Apache") {
-        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    } else {
-        return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-    }
-}
 
 // array of questions for user
 const questions = [{
